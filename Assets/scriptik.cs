fad6f53481c8c07f6fe8f3b109f1f75d3cb18c7f
Akinat0 <abu.amir@mail.ru>
2018-11-23 12:03:54 +0300

    Добавил scriptik

---
+++ Assets/scriptik.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptik : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// net pisuna
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
